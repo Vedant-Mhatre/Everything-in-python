dc040d5d29d79e9880ccd7d7c451ae08a079d9fb
vedant-mhatre <vedantmh@gmail.com>
2020-08-24 00:33:24 +0530

    added dfs

---
+++ graphs/Depth_First_Search/1dfs.py
@@ -0,0 +1,26 @@
+class graph:
+
+    def __init__(self,gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+def dfs(graph, start, visited = None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    
+    print(start)
+    
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return None
+
+gdict = { "a" : set(["b","c"]),
+                "b" : set(["a", "d"]),
+                "c" : set(["a", "d"]),
+                "d" : set(["e"]),
+                "e" : set(["a"])
+                }
+
+dfs(gdict, 'a')
