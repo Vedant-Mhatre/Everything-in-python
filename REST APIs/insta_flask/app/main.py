0c553aa8e8026718a3bf35e6b1a7eff78a81b3fb
vedant-mhatre <vedantmh@gmail.com>
2020-08-31 01:57:38 +0530

    connected mongodb

--- REST APIs/insta_flask/app/main.py
+++ REST APIs/insta_flask/app/main.py
@@ -1,21 +1,48 @@
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for, request, render_template
 from flask_restful import Api, Resource
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 api = Api(app)
 
-Users = {"user1": {"email":"user1@gmail.com", "username":"User 1"},
-		"user2": {"email":"user2@gmail.com", "username":"User 2"},
-		}
+client = MongoClient( os.environ['DB_PORT_27017_TCP_ADDR'],
+			27017)
+db = client.tododb
+
+Users = db.tododb.find()
+
+# Users = {"user1": {"email":"user1@gmail.com", "username":"User 1"},
+# 		"user2": {"email":"user2@gmail.com", "username":"User 2"},
+# 		}
 
 class User(Resource):
 	def get(self, name):
-		return Users[name]
+		if name not in Users:
+			return None
+		else:
+			return Users[name]
 
-	def post(self):
-		return {"userid": "0"}
+	def put(self, name):
+		# tempuser = {
+		# 	'username':name
+		#
+		# }
+		tempuser = {"user1": {"email":"user1@gmail.com", "username":"User 1"}}
+		print("before insert")
+
+		db.tododb.insert_one(tempuser)
+		print("after insert")
+		print({name})
+
+class Index(Resource):
+	def get(self):
+		users = [user for user in Users]
+		return users
 
 api.add_resource(User, "/<string:name>")
+api.add_resource(Index, "/")
 
 
 if __name__ == "__main__":
